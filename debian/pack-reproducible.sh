1550c21023a838b3cf7f88fe52c17fffb8d32ca1
Alexander Couzens <lynxis@fe80.eu>
2019-10-15 16:42:57 +0200

    debian: move vmlinuz out of the cpio
    To boot the image, the correct kernel is needed. Move the kernel image out of the cpio archive.

--- debian/pack-reproducible.sh
+++ debian/pack-reproducible.sh
@@ -13,5 +13,7 @@ fi
 tar c "--mtime=@$SOURCE_DATE_EPOCH" --numeric-owner -C "$ROOTDIR" . | bzip2 -c -9 > "$ARTIFACTDIR/$1.tar.bz2"
 cd "$ROOTDIR" || exit 1
 
+# move kernel out of the image
+mv "$ROOTDIR"/boot/vmlinuz* "$ARTIFACTDIR/$1.vmlinuz"
 find . | while read -r line ; do touch -hcd "@$SOURCE_DATE_EPOCH" "$line" ; done
 find . -mindepth 1 -printf '%P\0' | sort -z | cpio --reproducible -0 -o -H newc | gzip -9 -n > "$ARTIFACTDIR/$1.cpio.gz"
